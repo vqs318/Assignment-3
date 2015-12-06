69fc1d2495b13423e406e87acbd2c9303cccf700
Michael Milton <ttmigueltt@gmail.com>
2015-12-06 21:10:51 +0100

    Color cues for hover and observations

--- plot.js
+++ plot.js
@@ -6,7 +6,7 @@ d3.csv("first.txt", function(data){
 	// d3.csv("first.txt")
 	// 	.row(function(d) { return {key: d.key, value: +d.value}})
 	// 	//.get(function(error, rows));
-		
+	
 
 	var w=500;
 	var h=500;
@@ -24,28 +24,28 @@ d3.csv("first.txt", function(data){
 	};
 
 	var x = d3.scale.linear().range([0, w]);
-		x.domain([
-			d3.min(PCA, function(d) { return d[0]; }),
-		 	d3.max(PCA, function(d) { return d[0]; })
-	 	]);
-		
+	x.domain([
+		d3.min(PCA, function(d) { return d[0]; }),
+		d3.max(PCA, function(d) { return d[0]; })
+		]);
+	
 
-		var y = d3.scale.linear().range([h, 0]);
-		y.domain([
-			d3.min(PCA, function(d) { return d[1]; }),
-		 	d3.max(PCA, function(d) { return d[1]; })
-	 	]);
+	var y = d3.scale.linear().range([h, 0]);
+	y.domain([
+		d3.min(PCA, function(d) { return d[1]; }),
+		d3.max(PCA, function(d) { return d[1]; })
+		]);
 
 	var line = d3.svg.line()
-	    .x(function(d) { return x(d[0]); })
-	    .y(function(d) { return y(d[1]); })
+	.x(function(d) { return x(d[0]); })
+	.y(function(d) { return y(d[1]); })
 
 	var svg = d3.select("#pca")
-		.append("svg")
-		.attr("width",w +margin.left + margin.right)
-		.attr("height",h + margin.top + margin.bottom)
-		.append("g")
-		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	.append("svg")
+	.attr("width",w +margin.left + margin.right)
+	.attr("height",h + margin.top + margin.bottom)
+	.append("g")
+	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
 	svg
@@ -65,18 +65,28 @@ d3.csv("first.txt", function(data){
 			//console.log(y(d[1]))
 			return y(d[1]);
 		})
+		.style('transform-origin', function(d){
+			return x(d[0]) + 'px ' + y(d[1]) + 'px';
+		})
 		.attr("r",8)
 		.attr("class", function(d, i){
 			if ((i + 1) in observations)
 				return "data-connected-" + observations[i+1];
 			return "";
 		})
-	  	.on('mouseover', function(d, hand){
-   			updateHand(hand);
-	   	})
-	  	.append("svg:title")
-	   	.text(function(d,i) { return d[0] + ', ' + d[1] + '\n' + "This data point is at row index of " + (i+1); })
-	  	
+		.on('mouseover', function(d, hand){
+			d3.selectAll('.pca-point circle')
+			.classed('clicked', false);
+
+			d3.select(this)
+			.classed('clicked', true);
+
+	  		//Change hands
+	  		updateHand(hand);
+	  	})
+		.append("svg:title")
+		.text(function(d,i) { return d[0] + ', ' + d[1] + '\n' + "This data point is at row index of " + (i+1); })
+		
 
 
 	// svg.append("path")
@@ -87,34 +97,34 @@ d3.csv("first.txt", function(data){
 	//       .style("stroke", "black");
 
 	var xAxis = d3.svg.axis().scale(x)
-	    .orient("bottom")
-	    .tickFormat(function(d){
-	    	return d.toString();
-	    });
+	.orient("bottom")
+	.tickFormat(function(d){
+		return d.toString();
+	});
 
 	var yAxis = d3.svg.axis().scale(y)
-	    .orient("left");
+	.orient("left");
 
 	svg.append("g")
-		.attr("class", "x axis")
-		.attr("transform", "translate(0," + h + ")")
-		.call(xAxis);
-	   
+	.attr("class", "x axis")
+	.attr("transform", "translate(0," + h + ")")
+	.call(xAxis);
+	
 	svg.append("g")
-		.attr("class", "y axis")
-		.call(yAxis);
+	.attr("class", "y axis")
+	.call(yAxis);
 
 	svg.append("text")
-	    .style('text-anchor', 'middle')
-	    .attr("x", w/2)
-	    .attr("y", h + margin.top + margin.bottom/2)
-	    .text("First Principal Component");
+	.style('text-anchor', 'middle')
+	.attr("x", w/2)
+	.attr("y", h + margin.top + margin.bottom/2)
+	.text("First Principal Component");
 
-    svg.append("text")
-	    .style('text-anchor', 'middle')
-	    .attr("y", -margin.left/2)
-	    .attr("x", -h/2)
-	    .attr("transform", 'rotate(270)')
-	    .html("Second Principal Component");
+	svg.append("text")
+	.style('text-anchor', 'middle')
+	.attr("y", -margin.left/2)
+	.attr("x", -h/2)
+	.attr("transform", 'rotate(270)')
+	.html("Second Principal Component");
 
 });
